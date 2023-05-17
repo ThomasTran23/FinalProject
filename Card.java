c8eebbc692036e1cfeb71c2fc762e7b7470817e6
Thomas Tran <106930883+ThomasTran23@users.noreply.github.com>
2023-05-17 21:45:19 +0000

    lowkey not tryna do this shit

---
+++ Card.java
@@ -0,0 +1,19 @@
+public class Card{
+    private int rank;
+    private int suit;
+
+    public Card(int rank, int suit){
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public int getRank(){
+        return this.rank;
+    }
+
+    public int getSuit(){
+        return this.suit;
+    }
+
+    
+}
